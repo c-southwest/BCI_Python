65c5fcfa7e5793793dee0dc843b5c015db8a35f1
Nanxi Chen <41471673+c-southwest@users.noreply.github.com>
2022-05-20 22:27:01 +0800

    Update Generate_PSD_data.py
    change the language to English

--- Generate_PSD_data.py
+++ Generate_PSD_data.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from utils import Filters
 import os
 
-# 信号处理设定
+# Signal processing settings
 fs = 250
 nfft = 1024
 # Alpha Wave parameters
@@ -22,7 +22,7 @@ freq_left = 4
 freq_right = 23
 order = 5
 
-# 数据读写设定
+# Data r/w settings
 data_dir = "data/SSVEP_7_wet/"
 # Alpha Wave parameters
 # channels = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -34,31 +34,31 @@ slide_window = 0.5
 out_dir = "psd_data/SSVEP_7_wet/"
 
 for f in files:
-    # 读取一条10s的数据
+    # Read data
     fpath = data_dir + f
     print(fpath)
     data = DataFilter.read_file(fpath)
-    # 初始化时间窗口
+    # Initialize time window
     start = 1
     end = start + time_window * fs
     count = 0
     while start + time_window * fs * 0.8 < len(data[1]):
         psd_data = []
-        # 在给定时间段内，组合通道产生的psd数据
+        # Combine PSD data
         for index, channel in enumerate(channels):
             ch = data[channel][start:end]
             filtedData = Filters.BandStop(data=ch, low=48, high=52, order=order, fs=fs)
             filtedData2 = Filters.LowPass(data=filtedData, freq=freq_right, order=order, fs=fs)
             filtedData3 = Filters.HighPass(data=filtedData2, freq=freq_left, order=order, fs=fs)
             psd = welch(filtedData3, fs, nfft=nfft)
-            freq_end = 40  # 只关心40hz之前的PSD
+            freq_end = 40  # Only care PSD data before 40hz
             points = int((freq_end / (fs / 2)) * len(psd[0]))
             psd_data.append(psd[1][:points])
 
         np_psd = np.array(psd_data)
         np.save(out_dir + os.path.splitext(f)[0] + "_"+str(count), np_psd)
-        # 推进时间窗口
+        # Update time window
         start = start + int(fs*slide_window)
         end = start + time_window * fs
         count += 1
-    print("写入:", str(count + 1))
+    print("Write: ", str(count + 1))
